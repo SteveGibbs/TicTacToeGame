6e7cd75fddad323e5a649908c0826768e2a877d0
SteveGibbs <gibbsstephen@hotmail.com>
2016-08-12 15:51:58 +1000

    updated readme and beautified the world

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
 # TicTacToeGame
 
-First project of web development immersive (12 wk course) at General Assembly (Sydney).  Code will be cleaned up and improved over time as experience is gained.  
+First project (Project Zero) of web development immersive (12 wk course) at General Assembly (Sydney). Code was intentionally written long hand in javascript to better built up my fundamentals and syntax of that language and I purposefully avoided jQuery. My intention is to recode this project at a later time to reduce most of the repetition & redundancy in code and use jQuery to built up my knowledge of its syntax.
+
+## background
+
+## Features
+
+- two player mode
+- can change tokens:
+
+  - Classic - X vs O;
+  - Presidential - Hillary vs Trump;
+  - Day of the Dead - Mexican DofD images;
+  - Space - gifs of solarflare vs moon;
+
+    - tokens and backgrounds change
+
+- different messages for win by row, column or diagonal, or draw;
+- tokens originate on button click, then are disabled;
+- player scoreboard for wins;
+- reset game and reset scoreboard functionality;
+- single player vs computer AI (simple random choice); operates when button is clicked. Will add in harder AI functionality so you can choose which AI to play each turn. Set up for computer to go second at the moment.
+
+```javascript
+```
+
+## Usage
+
+## Technology
+
+- HTML5
+- CSS3
+- JavaScript
+
+### Libraries / Plugins
+
+- [jQuery](https://code.jquery.com/jquery-3.1.0.min.js)
+
+## Issues / Bugs
+
+- can select different tokens midway through game but this functionality is intentional but will need to built in logic so that you can remember the different tokens and match them (all first player vs second player) into game logic as a variant.
+- game allows you to continue after win if you do not select reset. May hardcode to prevent this.
+
+## Backlog / Futures Releases
+
+- will reduce redundancy in code and use jQuery as another version.
+- implement harder AI and have functionality to use harder or easier per turn.
+- add soundtracks per selection; just one soundtrack at the moment.
+
+## Acknowledgments
+
+- use of images for gifs and music audio.
--- main.js
+++ main.js
@@ -53,89 +53,86 @@ var startToken = tokenOne;
 var endToken = tokenTwo;
 var choice = 1;
 
-document.querySelector("#tokens").addEventListener("change",function() {
-  choice = parseInt( this.value );
+document.querySelector("#tokens").addEventListener("change", function() {
+  choice = parseInt(this.value);
 
   console.log("token change choice is number " + choice + " from dropdown menu");
 
-    document.querySelector("#tokens");
+  document.querySelector("#tokens");
   if (choice === 1) {
     startToken = tokenOne;
     endToken = tokenTwo;
     document.body.style.background = "#BDB48E";
-  }
-  else if (choice === 2){
+  } else if (choice === 2) {
     startToken = tokenThree;
     endToken = tokenFour;
     document.body.style.backgroundImage = "url('americanflag.gif')";
-  }
-  else if (choice ===3) {
+  } else if (choice === 3) {
     startToken = tokenFive;
     endToken = tokenSix;
     document.body.style.backgroundImage = "url('Day of the Dead.jpg')";
-    }
-  else if (choice ===4) {
+  } else if (choice === 4) {
     startToken = tokenSeven;
     endToken = tokenEight;
     document.body.style.backgroundImage = "url('niceimage.gif')";
-}
+  }
 });
 
 
 // Turn checker - if one token is selected, the other token must be played next
 
-var turnCheck = function () {
-count++;
-console.log("times pressed equals " + count);
-if (count%2===0) {
- whoseTurn = endToken;
-} else whoseTurn = startToken;
+var turnCheck = function() {
+  count++;
+  console.log("times pressed equals " + count);
+  if (count % 2 === 0) {
+    whoseTurn = endToken;
+  } else whoseTurn = startToken;
 };
 
 
 // Reset functionality - put all properties back to original state at board commencement
 document.getElementById("reset").onclick = function() {
-board.p1 = null;
-topRow1.innerText = "";
-board.p2 = null;
-topRow2.innerText = "";
-board.p3 = null;
-topRow3.innerText = "";
-board.p4 = null;
-secondRow4.innerText = "";
-board.p5 = null;
-secondRow5.innerText = "";
-board.p6 = null;
-secondRow6.innerText = "";
-board.p7 = null;
-thirdRow7.innerText = "";
-board.p8 = null;
-thirdRow8.innerText = "";
-board.p9 = null;
-thirdRow9.innerText = "";
-count = 0;
-whoseTurn = null;
-$("p1").remove();   // removes all messages on reset
-document.getElementById("topRow1").disabled = false;
-document.getElementById("topRow2").disabled = false;
-document.getElementById("topRow3").disabled = false;
-document.getElementById("secondRow4").disabled = false;
-document.getElementById("secondRow5").disabled = false;
-document.getElementById("secondRow6").disabled = false;
-document.getElementById("thirdRow7").disabled = false;
-document.getElementById("thirdRow8").disabled = false;
-document.getElementById("thirdRow9").disabled = false;
+  board.p1 = null;
+  topRow1.innerText = "";
+  board.p2 = null;
+  topRow2.innerText = "";
+  board.p3 = null;
+  topRow3.innerText = "";
+  board.p4 = null;
+  secondRow4.innerText = "";
+  board.p5 = null;
+  secondRow5.innerText = "";
+  board.p6 = null;
+  secondRow6.innerText = "";
+  board.p7 = null;
+  thirdRow7.innerText = "";
+  board.p8 = null;
+  thirdRow8.innerText = "";
+  board.p9 = null;
+  thirdRow9.innerText = "";
+  count = 0;
+  whoseTurn = null;
+  $("p1").remove(); // removes all messages on reset
+  document.getElementById("topRow1").disabled = false;
+  document.getElementById("topRow2").disabled = false;
+  document.getElementById("topRow3").disabled = false;
+  document.getElementById("secondRow4").disabled = false;
+  document.getElementById("secondRow5").disabled = false;
+  document.getElementById("secondRow6").disabled = false;
+  document.getElementById("thirdRow7").disabled = false;
+  document.getElementById("thirdRow8").disabled = false;
+  document.getElementById("thirdRow9").disabled = false;
 };
 
 // Reset Score functionality
 
 document.getElementById("resetScore").onclick = function() {
-startPlayer = null;
-var b = document.querySelector(".score2");
-b.innerHTML=startPlayer;
-secondPlayer = null;
-var c = document.querySelector(".score4");
-c.innerHTML=secondPlayer;
+  startPlayer = null;
+  var b = document.querySelector(".score2");
+  b.innerHTML = startPlayer;
+  secondPlayer = null;
+  var c = document.querySelector(".score4");
+  c.innerHTML = secondPlayer;
 };
 
 // Put token into board on button click.  Select element by id, token is equal to
@@ -143,94 +140,94 @@ c.innerHTML=secondPlayer;
 // or a draw or else continue.  Disable button once token has been parsed through into object
 
 document.getElementById("topRow1").onclick = function() {
-turnCheck();
-board.p1 = whoseTurn;
-topRow1.innerHTML=whoseTurn;
-gameCheck();
-if (board.p1!==null){
-document.getElementById("topRow1").disabled = true;
-}
+  turnCheck();
+  board.p1 = whoseTurn;
+  topRow1.innerHTML = whoseTurn;
+  gameCheck();
+  if (board.p1 !== null) {
+    document.getElementById("topRow1").disabled = true;
+  }
 };
 
 document.getElementById("topRow2").onclick = function() {
-turnCheck();
-board.p2 = whoseTurn;
-topRow2.innerHTML=whoseTurn;
-gameCheck();
-if (board.p2!==null){
-document.getElementById("topRow2").disabled = true;
-}
+  turnCheck();
+  board.p2 = whoseTurn;
+  topRow2.innerHTML = whoseTurn;
+  gameCheck();
+  if (board.p2 !== null) {
+    document.getElementById("topRow2").disabled = true;
+  }
 };
 
 document.getElementById("topRow3").onclick = function() {
-turnCheck();
-board.p3 = whoseTurn;
-topRow3.innerHTML=whoseTurn;
-gameCheck();
-if (board.p3!==null){
-document.getElementById("topRow3").disabled = true;
-}
+  turnCheck();
+  board.p3 = whoseTurn;
+  topRow3.innerHTML = whoseTurn;
+  gameCheck();
+  if (board.p3 !== null) {
+    document.getElementById("topRow3").disabled = true;
+  }
 };
 
 document.getElementById("secondRow4").onclick = function() {
-turnCheck();
-board.p4 = whoseTurn;
-secondRow4.innerHTML=whoseTurn;
-gameCheck();
-if (board.p4!==null){
-document.getElementById("secondRow4").disabled = true;
-}
+  turnCheck();
+  board.p4 = whoseTurn;
+  secondRow4.innerHTML = whoseTurn;
+  gameCheck();
+  if (board.p4 !== null) {
+    document.getElementById("secondRow4").disabled = true;
+  }
 };
 
 document.getElementById("secondRow5").onclick = function() {
-turnCheck();
-board.p5 = whoseTurn;
-secondRow5.innerHTML=whoseTurn;
-gameCheck();
-if (board.p5!==null){
-document.getElementById("secondRow5").disabled = true;
-}
+  turnCheck();
+  board.p5 = whoseTurn;
+  secondRow5.innerHTML = whoseTurn;
+  gameCheck();
+  if (board.p5 !== null) {
+    document.getElementById("secondRow5").disabled = true;
+  }
 };
 
 document.getElementById("secondRow6").onclick = function() {
-turnCheck();
-board.p6 = whoseTurn;
-secondRow6.innerHTML=whoseTurn;
-gameCheck();
-if (board.p6!==null){
-document.getElementById("secondRow6").disabled = true;
-}
+  turnCheck();
+  board.p6 = whoseTurn;
+  secondRow6.innerHTML = whoseTurn;
+  gameCheck();
+  if (board.p6 !== null) {
+    document.getElementById("secondRow6").disabled = true;
+  }
 };
 
 document.getElementById("thirdRow7").onclick = function() {
-turnCheck();
-board.p7 = whoseTurn;
-thirdRow7.innerHTML=whoseTurn;
-gameCheck();
-if (board.p7!==null){
-document.getElementById("thirdRow7").disabled = true;
-}
+  turnCheck();
+  board.p7 = whoseTurn;
+  thirdRow7.innerHTML = whoseTurn;
+  gameCheck();
+  if (board.p7 !== null) {
+    document.getElementById("thirdRow7").disabled = true;
+  }
 };
 
 document.getElementById("thirdRow8").onclick = function() {
-turnCheck();
-board.p8 = whoseTurn;
-thirdRow8.innerHTML=whoseTurn;
-gameCheck();
-if (board.p8!==null){
-document.getElementById("thirdRow8").disabled = true;
-}
+  turnCheck();
+  board.p8 = whoseTurn;
+  thirdRow8.innerHTML = whoseTurn;
+  gameCheck();
+  if (board.p8 !== null) {
+    document.getElementById("thirdRow8").disabled = true;
+  }
 };
 
 
 document.getElementById("thirdRow9").onclick = function() {
-turnCheck();
-board.p9 = whoseTurn;
-thirdRow9.innerHTML=whoseTurn;
-gameCheck();
-if (board.p9!==null){
-document.getElementById("thirdRow9").disabled = true;
-}
+  turnCheck();
+  board.p9 = whoseTurn;
+  thirdRow9.innerHTML = whoseTurn;
+  gameCheck();
+  if (board.p9 !== null) {
+    document.getElementById("thirdRow9").disabled = true;
+  }
 
 };
 
@@ -240,138 +237,136 @@ document.getElementById("thirdRow9").disabled = true;
 // need first condition does not equal null as each p starts as a null otherwise
 // they all equal each other
 
-var gameCheck = function(){
+var gameCheck = function() {
 
-//check ROWS
-if (board.p1!==null&&board.p1===board.p2&&board.p2===board.p3) {
-  rowMessage();
-  if (board.p1===startToken) {
-    startPlayer++;
-    startPlayerScore();
+  //check ROWS
+  if (board.p1 !== null && board.p1 === board.p2 && board.p2 === board.p3) {
+    rowMessage();
+    if (board.p1 === startToken) {
+      startPlayer++;
+      startPlayerScore();
 
-  } else {
-    secondPlayer++;
-    secondPlayerScore();
+    } else {
+      secondPlayer++;
+      secondPlayerScore();
+    }
+
+  } else if (board.p4 !== null && board.p4 === board.p5 && board.p5 === board.p6) {
+    rowMessage();
+    if (board.p4 === startToken) {
+      startPlayer++;
+      startPlayerScore();
+    } else {
+      secondPlayer++;
+      secondPlayerScore();
+    }
+  } else if (board.p7 !== null && board.p7 === board.p8 && board.p8 === board.p9) {
+    rowMessage();
+    if (board.p7 === startToken) {
+      startPlayer++;
+      startPlayerScore();
+    } else {
+      secondPlayer++;
+      secondPlayerScore();
+    }
+  }
+  // check COLUMNS
+  else if (board.p1 !== null && board.p1 === board.p4 && board.p4 === board.p7) {
+    columnMessage();
+    if (board.p1 === startToken) {
+      startPlayer++;
+      startPlayerScore();
+    } else {
+      secondPlayer++;
+      secondPlayerScore();
+    }
+
+
+  } else if (board.p2 !== null && board.p2 === board.p5 && board.p5 === board.p8) {
+    columnMessage();
+    if (board.p2 === startToken) {
+      startPlayer++;
+      startPlayerScore();
+    } else {
+      secondPlayer++;
+      secondPlayerScore();
+    }
+  } else if (board.p3 !== null && board.p3 === board.p6 && board.p6 === board.p9) {
+    columnMessage();
+    if (board.p3 === startToken) {
+      startPlayer++;
+      startPlayerScore();
+    } else {
+      secondPlayer++;
+      secondPlayerScore();
+    }
   }
 
-} else if (board.p4!==null&&board.p4===board.p5&&board.p5===board.p6) {
-  rowMessage();
-  if (board.p4===startToken) {
-    startPlayer++;
-    startPlayerScore();
-  } else {
-    secondPlayer++;
-    secondPlayerScore();
+  // CHECK DIAGONALS
+  else if (board.p1 !== null & board.p1 === board.p5 && board.p5 === board.p9) {
+    diagonalMessage();
+    if (board.p1 === startToken) {
+      startPlayer++;
+      startPlayerScore();
+    } else {
+      secondPlayer++;
+      secondPlayerScore();
+    }
+  } else if (board.p3 !== null & board.p3 === board.p5 && board.p5 === board.p7) {
+    diagonalMessage();
+    if (board.p3 === startToken) {
+      startPlayer++;
+      startPlayerScore();
+    } else {
+      secondPlayer++;
+      secondPlayerScore();
+    }
   }
-} else if (board.p7!==null&&board.p7===board.p8&&board.p8===board.p9) {
-  rowMessage();
-  if (board.p7===startToken) {
-    startPlayer++;
-    startPlayerScore();
-  } else {
-    secondPlayer++;
-    secondPlayerScore();
-  }
-}
-// check COLUMNS
-else if (board.p1!==null&&board.p1===board.p4&&board.p4===board.p7) {
-  columnMessage();
-  if (board.p1===startToken) {
-    startPlayer++;
-    startPlayerScore();
-  } else {
-    secondPlayer++;
-    secondPlayerScore();
-  }
-
-
-} else if (board.p2!==null&&board.p2===board.p5&&board.p5===board.p8) {
-  columnMessage();
-  if (board.p2===startToken) {
-    startPlayer++;
-    startPlayerScore();
-  } else {
-    secondPlayer++;
-    secondPlayerScore();
-  }
-}
-else if (board.p3!==null&&board.p3===board.p6&&board.p6===board.p9) {
-  columnMessage();
-  if (board.p3===startToken) {
-    startPlayer++;
-    startPlayerScore();
-  } else {
-    secondPlayer++;
-    secondPlayerScore();
-  }
-}
-
-// CHECK DIAGONALS
-else if (board.p1!==null&board.p1===board.p5&&board.p5===board.p9) {
-  diagonalMessage();
-  if (board.p1===startToken) {
-    startPlayer++;
-    startPlayerScore();
-  } else {
-    secondPlayer++;
-    secondPlayerScore();
-  }
-}
-else if (board.p3!==null&board.p3===board.p5&&board.p5===board.p7) {
-  diagonalMessage();
-  if (board.p3===startToken) {
-    startPlayer++;
-    startPlayerScore();
-  } else {
-    secondPlayer++;
-    secondPlayerScore();
-  }
-}
-//CHECK DRAW OR CONTINUE
-else if (count===9) {
+  //CHECK DRAW OR CONTINUE
+  else if (count === 9) {
     drawMessage();
-}
- else {
-  console.log("game is still on");
-}};
+  } else {
+    console.log("game is still on");
+  }
+};
 
 // MESSAGES FOR ROW WIN, COLUMN WIN, DIAGONAL WIN OR DRAW
-rowMessage = function(){
-var createRowMessage = document.createElement("p1");                        // Create a <p> node
-var textRowMessage = document.createTextNode("Winner - three in a row!");   // Create a text node
-createRowMessage.appendChild(textRowMessage);                               // Append the text to <p>
-document.body.appendChild(createRowMessage);
+rowMessage = function() {
+  var createRowMessage = document.createElement("p1"); // Create a <p> node
+  var textRowMessage = document.createTextNode("Winner - three in a row!"); // Create a text node
+  createRowMessage.appendChild(textRowMessage); // Append the text to <p>
+  document.body.appendChild(createRowMessage);
 };
 
-columnMessage = function(){
-var createColumnMessage = document.createElement("p1");                        // Create a <p> node
-var textColumnMessage = document.createTextNode("Winner - three in a column!"); // Create a text node
-createColumnMessage.appendChild(textColumnMessage);                           // Append the text to <p>
-document.body.appendChild(createColumnMessage);
+columnMessage = function() {
+  var createColumnMessage = document.createElement("p1"); // Create a <p> node
+  var textColumnMessage = document.createTextNode("Winner - three in a column!"); // Create a text node
+  createColumnMessage.appendChild(textColumnMessage); // Append the text to <p>
+  document.body.appendChild(createColumnMessage);
 };
 
-diagonalMessage = function(){
-var createDiagonalMessage = document.createElement("p1");                        // Create a <p> node
-var textDiagonalMessage = document.createTextNode("Winner - three diagonally!"); // Create a text node
-createDiagonalMessage.appendChild(textDiagonalMessage);                         // Append the text to <p>
-document.body.appendChild(createDiagonalMessage);
+diagonalMessage = function() {
+  var createDiagonalMessage = document.createElement("p1"); // Create a <p> node
+  var textDiagonalMessage = document.createTextNode("Winner - three diagonally!"); // Create a text node
+  createDiagonalMessage.appendChild(textDiagonalMessage); // Append the text to <p>
+  document.body.appendChild(createDiagonalMessage);
 };
 
-drawMessage = function(){
-var createDrawMessage = document.createElement("p1");                        // Create a <p> node
-var textDrawMessage = document.createTextNode("No glory but a hard earned draw!"); // Create a text node
-createDrawMessage.appendChild(textDrawMessage);                             // Append the text to <p>
-document.body.appendChild(createDrawMessage);
+drawMessage = function() {
+  var createDrawMessage = document.createElement("p1"); // Create a <p> node
+  var textDrawMessage = document.createTextNode("No glory but a hard earned draw!"); // Create a text node
+  createDrawMessage.appendChild(textDrawMessage); // Append the text to <p>
+  document.body.appendChild(createDrawMessage);
 };
 
-startPlayerScore = function(){
-var b = document.querySelector(".score2");
-b.innerHTML=startPlayer;
+startPlayerScore = function() {
+  var b = document.querySelector(".score2");
+  b.innerHTML = startPlayer;
 };
 
-secondPlayerScore = function(){
-var c = document.querySelector(".score4");
-c.innerHTML=secondPlayer;
+secondPlayerScore = function() {
+  var c = document.querySelector(".score4");
+  c.innerHTML = secondPlayer;
 };
 
 // ARTIFICIAL INTELLIGENCE LOGIC - COMPUTER SELECTS ON BUTTON CLICK - ONLY WORKS FOR SECOND PLAYER
@@ -379,66 +374,66 @@ c.innerHTML=secondPlayer;
 
 var playerChoice;
 document.getElementById("singlePlayer").onclick = function() {
-  playerChoice = parseInt( this.value );
-  if (count%2!==0) {
-      computer();
-        }
-      };
+  playerChoice = parseInt(this.value);
+  if (count % 2 !== 0) {
+    computer();
+  }
+};
 
 var computer = function() {
-var boardKeys = Object.keys(board);
-var newArray = [];
+  var boardKeys = Object.keys(board);
+  var newArray = [];
 
-for (var i=0; i<boardKeys.length; i++) {
-  if (board[boardKeys[i]]===null) {
-    newArray.push(boardKeys[i]);
+  for (var i = 0; i < boardKeys.length; i++) {
+    if (board[boardKeys[i]] === null) {
+      newArray.push(boardKeys[i]);
+    }
   }
-}
-var randomChoice = newArray[Math.floor(newArray.length * Math.random())];
-board[randomChoice] = endToken;
-  if (randomChoice==="p1") {
+  var randomChoice = newArray[Math.floor(newArray.length * Math.random())];
+  board[randomChoice] = endToken;
+  if (randomChoice === "p1") {
     topRow1.innerHTML = endToken;
     turnCheck();
     gameCheck();
   }
-  if (randomChoice==="p2") {
+  if (randomChoice === "p2") {
     topRow2.innerHTML = endToken;
     turnCheck();
     gameCheck();
   }
-  if (randomChoice==="p3") {
+  if (randomChoice === "p3") {
     topRow3.innerHTML = endToken;
     turnCheck();
     gameCheck();
   }
-  if (randomChoice==="p4") {
+  if (randomChoice === "p4") {
     secondRow4.innerHTML = endToken;
     turnCheck();
     gameCheck();
   }
-  if (randomChoice==="p5") {
+  if (randomChoice === "p5") {
     secondRow5.innerHTML = endToken;
     turnCheck();
     gameCheck();
   }
-  if (randomChoice==="p6") {
+  if (randomChoice === "p6") {
     secondRow6.innerHTML = endToken;
     turnCheck();
     gameCheck();
   }
-  if (randomChoice==="p7") {
+  if (randomChoice === "p7") {
     thirdRow7.innerHTML = endToken;
     turnCheck();
     gameCheck();
   }
-  if (randomChoice==="p8") {
+  if (randomChoice === "p8") {
     thirdRow8.innerHTML = endToken;
     turnCheck();
     gameCheck();
   }
-  if (randomChoice==="p9") {
+  if (randomChoice === "p9") {
     thirdRow9.innerHTML = endToken;
     turnCheck();
     gameCheck();
   }
-  };
+};
